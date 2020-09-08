70efc397ef2bcc03fe3b3229d1e71ed9a876e6ce
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2020-09-08 23:42:19 +0530

    Day-017

---
+++ Day-017/odd_list.py
@@ -0,0 +1,9 @@
+def Odd_List(lst):
+    l=[]
+    for i in range(len(lst)):
+        if lst[i]%2!=0:
+            l.append(lst[i])
+    print(l)
+
+lst=[int(x) for x in input().split()]
+Odd_List(lst)
